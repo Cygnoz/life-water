87579b5f7f63efa881abe7898c8ba000ee24364b
axsaje <axsaje@U-3S2CWBQE0L5HJ.vdi.cygnoz.com>
2024-09-25 16:59:02 +0530

    connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-console.log(helo);
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+require('./db/connection');
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
