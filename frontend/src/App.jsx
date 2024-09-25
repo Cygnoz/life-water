92e7bf2b53a24f1a1d29e2b6e5fff9729c687aa4
axmuku <axmuku@U-35LICS3RPCOYA.vdi.cygnoz.com>
2024-09-25 11:10:06 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className="App text-red-400">
+tdghfg
+</div>
+    </>
+  )
+}
+
+export default App
