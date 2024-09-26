4102bef872d6c2e170c41ce05aee3a9d15f3bc0a
SanalCygnoz <sanal.k@cygnonex.com>
2024-09-26 14:54:57 +0530

    route sett admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom'
+import Createorder from './pages/Createorder'
 
 function App() {
 
 
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        
+    <Routes>
+    <Route path='/createorder' element={<Createorder/>} />
 
+    </Routes>
     </> 
   )
 }
