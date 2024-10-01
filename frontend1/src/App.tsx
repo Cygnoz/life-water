27c2a5d7e9c7b665fa9c8a064e64517100e1c696
abhinuak1 <abhinu.raghunathan@cygnonex.com>
2024-10-01 11:02:51 +0530

    view and sidebar

--- frontend1/src/App.tsx
+++ frontend1/src/App.tsx
@@ -5,6 +5,10 @@ import Customers from './pages/Customers';
 
 import Returncustomer from './pages/Returncustomer';
 import { useState } from 'react';
+import Visithistory from './pages/VisitHistory';
+import AddPayment from './pages/AddPayment';
+import Summary from './pages/Summary';
+import PaymentCollection from './pages/PaymentCollection';
 
 const App: React.FC = () => {
   const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
@@ -21,8 +25,11 @@ const App: React.FC = () => {
         <div className="flex-grow p-5 pt-[60px]">
           <Routes>
             <Route path="/customers" element={<Customers />} />
-       
             <Route path="/return" element={<Returncustomer />} />
+            <Route path="/visithistory" element={<Visithistory />} />
+            <Route path="/addpayment" element={<AddPayment />} />
+            <Route path="/collection" element={<PaymentCollection />} />
+            <Route path="/summary" element={<Summary />} />
           </Routes>
         </div>
       </div>
