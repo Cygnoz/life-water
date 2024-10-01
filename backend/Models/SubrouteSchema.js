d29fedde4a920bdbf857e5291606d44afdf5fdde
Akhil Sajeevan <akhilsajeevan@cygnonex.com>
2024-10-01 15:23:48 +0530

    subroute

--- backend/Models/SubrouteSchema.js
+++ backend/Models/SubrouteSchema.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Route = require('./RouteSchema'); // Importing the Route schema
 
-const subrouteSchema = new Schema({
+const subRouteSchema = new Schema({
   subrouteName: {
     type: String,
     required: true
@@ -11,15 +12,17 @@ const subrouteSchema = new Schema({
     required: true,
     unique: true
   },
-  routeCode: {
-    type: String,
-    required: true, // Referencing the route by routeCode
-    ref: 'Route'
-  },
   description: {
     type: String
+  },
+  routeCode: {
+    type: String,
+    required: true,
+    ref: 'Route'  // Reference to Route schema's routeCode
   }
+}, {
+  timestamps: true  // This adds createdAt and updatedAt fields
 });
 
-const Subroute = mongoose.model('Subroute', subrouteSchema);
-module.exports = Subroute;
+const SubRoute = mongoose.model('SubRoute', subRouteSchema);
+module.exports = SubRoute;
