d1aa3a8d9e2420751cdc8a8350f1d1fb23114f26
arjunvm1 <148052201+arjunvm1@users.noreply.github.com>
2024-10-02 13:23:32 +0530

    completed addstaff sunctionality

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,8 +5,10 @@ const vehicleController = require('../controller/VehicleController');
 const routeController = require('../controller/RouteController');
 const subrouteController = require('../controller/SubRouteController');
 
+const upload = require('../middleware/Multermiddleware')
+
 // Add staff
-router.post('/staff', staffController.addStaff);
+router.post('/addstaff',upload.single('profile'),staffController.addStaff);
 
 // Get all staff
 router.get('/getallstaff', staffController.getAllStaff);
