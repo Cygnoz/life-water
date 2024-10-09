a7ce67f953b024b0c489b3aa6f524f68bf69cfcb
arjunvm1 <148052201+arjunvm1@users.noreply.github.com>
2024-10-09 12:56:28 +0530

    minor push

--- backend/controller/staffController.js
+++ backend/controller/staffController.js
@@ -1,6 +1,8 @@
-const Staff = require('../models/staffSchema'); // Adjust the path based on your file structure
+const Staff = require("../models/StaffSchema");
+
 
 // Add new staff
+
 
 
 // Get all staff members
