25b4a9a83afd0f693844ce3d31ac498660c9496b
sreekrishnan209 <sreekrishnan209@gmail.com>
2024-10-22 10:17:18 +0530

    changed

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,11 +5,11 @@ const vehicleController = require('../controller/VehicleController');
 const routeController = require('../controller/RouteController');
 const subrouteController = require('../controller/SubRouteController');
 const CustomerController = require('../controller/CustomerController');
- 
+
 const upload = require('../middleware/Multermiddleware')
- 
-// STAFF
- 
+
+// STAFF 
+
 // Add staff
 router.post('/addstaff',upload.single('profile'),staffController.addStaff);
 // Get all staff
@@ -20,33 +20,33 @@ router.get('/staff/:id', staffController.getStaffById);
 router.put('/staff/:id',upload.single('profile'), staffController.editStaff);
 // Delete staff
 router.delete('/staff/:id', staffController.deleteStaff);
- 
- 
-// VEHICLE
-// Add vehicle
+
+
+// VEHICLE 
+// Add vehicle 
 router.post('/addVehicle', upload.single('vehicleImage'), vehicleController.addVehicle);
-// view vehicle
+// view vehicle 
 router.get('/viewVehicles', vehicleController.getAllVehicles);
-// view single vehicle
+// view single vehicle 
 router.get('/viewvehicle/:id',vehicleController.viewVehicleById)
-// edit vehicle
+// edit vehicle 
 router.put('/editvehicle/:id',upload.single('image'), vehicleController.updateVehicle);
-// delete vehicle
+// delete vehicle 
 router.delete('/deletevehicle/:id', vehicleController.deleteVehicle);
- 
- 
- 
- 
- 
-// ROUTE
- 
+
+
+
+
+
+// ROUTE 
+
 // Route endpoints
 router.post('/addRoute', routeController.addRoute);
 router.delete('/delRoute/:id', routeController.deleteRoute);    // Delete route by ObjectId
 router.put('/updateRoute/:id', routeController.updateRoute);       // Update route by ObjectId
 router.get('/getAllRoutes' ,routeController.getAllRoutes)        // View all routes
 router.get('/getroute/:id', routeController.viewRouteById);     // View route by ObjectId
- 
+
 // subroute
 router.post('/addSRoute', subrouteController.addSubroute);
 router.put('/updateSRoute/:id', subrouteController.editSubroute);
@@ -54,22 +54,20 @@ router.delete('/delSRoute/:id', subrouteController.deleteSubroute);
 router.get('/viewSRoute', subrouteController.viewAllSubroutes);
 router.get('/viewSRoute/:id', subrouteController.viewSubroute);
 router.get('/getSRoute/:id', subrouteController.getSubroutebyID);
- 
+
 //customer
 // Bussiness customer
-router.post('/addcustomer', upload.single('logo'),CustomerController.createCustomer);
 // router.post('/business-customer',  upload.single('logo'),CustomerController.createBusinessCustomer);
-// router.get('/business-customer', CustomerController.getAllBusinessCustomers);
-// router.get('/business-customer/:id', CustomerController.getBusinessCustomerById);
-// router.put('/business-customer/:id', CustomerController.updateBusinessCustomerById);
-// router.delete('/business-customer/:id', CustomerController.deleteBusinessCustomerById);
- 
-// // Individual customer
-// // router.post('/customer', CustomerController.createCustomer);
+router.get('/business-customer', CustomerController.getAllBusinessCustomers);
+router.get('/business-customer/:id', CustomerController.getBusinessCustomerById);
+router.put('/business-customer/:id', CustomerController.updateBusinessCustomerById);
+router.delete('/business-customer/:id', CustomerController.deleteBusinessCustomerById);
+
+// Individual customer
+router.post('/customer', CustomerController.createCustomer);
 router.get('/customer', CustomerController.getAllCustomers);
 router.get('/customer/:id', CustomerController.getCustomerById);
 router.put('/customer/:id', CustomerController.updateCustomerById);
 router.delete('/customer/:id', CustomerController.deleteCustomerById);
- 
+
 module.exports = router;
- 
