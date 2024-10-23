595e69a86bca297e26733dddceb7090a8c342077
abhinuak1 <abhinu.raghunathan@cygnonex.com>
2024-10-23 18:30:39 +0530

    login salesman page

--- frontend1/src/App.tsx
+++ frontend1/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Customers from './pages/Customers';
-
 import Returncustomer from './pages/Returncustomer';
 import { useState } from 'react';
 import Visithistory from './pages/VisitHistory';
-
 import Summary from './pages/Summary';
 import PaymentCollection from './pages/PaymentCollection';
 import Addreturncustomer from './components/Addreturncustomer';
@@ -14,37 +12,41 @@ import Stock from './pages/Stock';
 import Couponsale from './components/CouponSale';
 import CouponCustomer from './pages/CouponCustomer';
 import CreditCollection from './components/CreditCollection';
+import Login from './login/Login';
 
 const App: React.FC = () => {
   const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const location = useLocation(); // Get the current path
 
   const handleToggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
 
   return (
-    <BrowserRouter>
-      <div className="flex">
+    <div className="flex">
+      {/* Conditionally render the Sidebar if not on the login page */}
+      {location.pathname !== '/' && (
         <Sidebar isSidebarOpen={isSidebarOpen} handleToggleSidebar={handleToggleSidebar} />
-        
-        <div className="flex-grow pt-[60px]">
-          <Routes>
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/return" element={<Returncustomer />} />
-            <Route path="/visithistory" element={<Visithistory />} />
-            <Route path="/addpayment" element={<Addreturncustomer/>} />
-            <Route path="/collection" element={<PaymentCollection />} />
-            <Route path="/summary" element={<Summary />} />
-            <Route path="/stock" element={<Stock />} />
-            <Route path="/couponsale" element={<Couponsale />} />
-            <Route path="/couponcustomer" element={<CouponCustomer />} />
-            <Route path="/creditcollection" element={<CreditCollection />} />
-          </Routes>
-        </div>
+      )}
+
+      {/* Conditionally apply margin-top if not on login page */}
+      <div className={`flex-grow ${location.pathname !== '/' ? 'mt-[60px]' : ''}`}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/return" element={<Returncustomer />} />
+          <Route path="/visithistory" element={<Visithistory />} />
+          <Route path="/addpayment" element={<Addreturncustomer />} />
+          <Route path="/collection" element={<PaymentCollection />} />
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/stock" element={<Stock />} />
+          <Route path="/couponsale" element={<Couponsale />} />
+          <Route path="/couponcustomer" element={<CouponCustomer />} />
+          <Route path="/creditcollection" element={<CreditCollection />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
-
