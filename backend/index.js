5994559abaec4f0794dab2486c97f07b09aaf032
Akhil Sajeevan <akhilsajeevan@cygnonex.com>
2024-11-05 16:53:56 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,21 @@ const vehicleRoute = require('./routes/router.js');
 
 // Middleware
 const corsOptions = {
-  origin: 'http://3.110.171.51:4173', // Your frontend origin
-  optionsSuccessStatus: 200
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://3.110.171.51:5173', 'http://3.110.171.51:4173'];
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true, // If using cookies or authorization headers
 };
+ 
 app.use(cors(corsOptions));
+
+// app.use(cors())
 app.use(express.json());
 
 app.use('/uploads', express.static('uploads')); // Ensure the path is correct
